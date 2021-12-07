bc35db4df694cc634d9028b6ac93fb60da5d4af6
alberto <melchorherrera@gmail.com>
2021-12-07 14:25:19 +0100

    first commit

--- html/dbmanager/server/nodejs/cfg/generalcfg.js
+++ html/dbmanager/server/nodejs/cfg/generalcfg.js
@@ -2,6 +2,6 @@ import config from './generalcfgdefaults.js'
 export default config;
 
 //config.themesPath="./client/themes";
-config.serverPort="8010";
+config.serverPort="8020";
 //config.masterPassword="password";
 //config.autoLogin="systemadmin";
